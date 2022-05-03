16fb9896ced483194d1cbd622ec9264f5367749f
Adelfjj <adelsonfranciscojj@outlook.com>
2022-05-03 20:47:43 -0300

    modal login button

--- script/script.js
+++ script/script.js
@@ -2,7 +2,17 @@
 var toggle = document.querySelector('.menu-mobile')
    .addEventListener('click',function(){
       this.classList.toggle("change");
-
-      
       var nav = document.querySelector('nav').classList.toggle("slide");
 });
+
+var userLogin = document.querySelector('#login')
+   .addEventListener('click',function(){
+
+     var drop_login = document.querySelector('.drop-login');
+     if(drop_login.style.display === 'block')
+     drop_login.style.display = 'none';
+     else{
+      drop_login.style.display = 'block';
+     }
+     
+   });
